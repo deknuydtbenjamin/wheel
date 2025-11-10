d0643516324559fba0c7c7c748363c48074574ab
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-11-10 14:17:55 +0100

    fix: bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { createBrowserRouter, Router, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
 const router = createBrowserRouter([
   {
