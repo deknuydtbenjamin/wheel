097abcf4ad978e7944acbd11732610e94f1a9f1e
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2025-11-17 11:10:50 +0100

    feat: create type

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+export type JeuxType ={
+    id: number;
+    name: string;
+    img?: string;
+}
+
+export const jeux: JeuxType[] =[
+    {
+        id: 1,
+        name: "Banco"
+    },
+    {
+        id: 2,
+        name: "Goal"
+    },
+    {
+        id: 3,
+        name: "Cash"
+    },
+]
